a56eff9a8103d24f298ad6a1e566e7940a0ae338
Unreallx <artemx2224@yandex@gmail.com>
2025-05-26 21:53:26 +0300

    localization add

---
+++ localization/quanta_en.ts
@@ -0,0 +1,167 @@
+<TS version="2.1" language="en_US">
+<context>
+    <name>Main</name>
+    <message>
+        <source>Main</source>
+        <translation>Main</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <source>Parameters</source>
+        <translation>Parameters</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+
+    <message>
+        <source>Cleared</source>
+        <translation>Can be cleared ~ </translation>
+    </message>
+    <message>
+        <source>WaitingTask</source>
+        <translation>Waiting for tasks...</translation>
+    </message>
+
+	
+    <message>
+        <source>ClearedTemporary</source>
+        <translation>Cleared temporary files on</translation>
+    </message>
+    <message>
+    	<source>AdminWarning</source>
+  	<translation>Run as administrator</translation>
+    </message>
+    <message>
+    	<source>WinSXS</source>
+  	<translation>Cleared in WinSxS on</translation>
+    </message>
+    <message>
+    	<source>ClearedWinTemp</source>
+  	<translation>Cleared in WinTemp on</translation>
+    </message>
+    <message>
+    	<source>ClearedFontCache</source>
+  	<translation>Cleared fontcache files on</translation>
+    </message>
+    <message>
+    	<source>ClearedBin</source>
+  	<translation>Cleared recycle bin on</translation>
+    </message>
+    <message>
+    	<source>ClearedUpdate</source>
+  	<translation>Cleared cache updates on</translation>
+    </message>
+    <message>
+    	<source>ClearedEvent</source>
+  	<translation>Cleared event logs on</translation>
+    </message>
+    <message>
+    	<source>ClearedDump</source>
+  	<translation>Cleared error dump on</translation>
+    </message>
+    <message>
+    	<source>ClearedPoint</source>
+  	<translation>Cleared garbage from 
+recovery points on</translation>
+    </message>
+
+</context>
+
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <source>ThisLang</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <source>ThemeMode</source>
+        <translation>Theme mode</translation>
+    </message>
+    <message>
+        <source>DarkTheme</source>
+        <translation>Dark theme</translation>
+    </message>
+    <message>
+        <source>LightTheme</source>
+        <translation>Light theme</translation>
+    </message>
+    <message>
+        <source>Animations</source>
+        <translation>Animations</translation>
+    </message>
+    <message>
+        <source>Autoloading</source>
+        <translation>Auto-loading</translation>
+    </message>
+   <message>
+        <source>Report</source>
+        <translation>Report an error</translation>
+    </message>
+    <message>
+        <source>Notification</source>
+        <translation>Location of notifications</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Debug mode</translation>
+    </message>
+    <message>
+        <source>ChooseThemeMode</source>
+        <translation>Choose theme mode</translation>
+    </message>
+    <message>
+        <source>ChooseLang</source>
+        <translation>Choose language</translation>
+    </message>
+    <message>
+        <source>Copied</source>
+        <translation>Copied successfully!</translation>
+    </message>
+    <message>
+        <source>FindError</source>
+        <translation>Did you find a mistake?</translation>
+    </message>
+    <message>
+        <source>Support</source>
+        <translation>Contact support</translation>
+    </message>
+</context>
+
+
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>WaitingTask</source>
+        <translation>Waiting for tasks...</translation>
+    </message>
+    <message>
+        <source>CleaningAnalysis</source>
+        <translation>Cleaning analysis...</translation>
+    </message>
+</context>
+
+
+<context>
+    <name>ParametersPage</name>
+    <message>
+        <source>Info</source>
+        <translation>info</translation>
+    </message>
+</context>
+
+
+
+</TS>
---
+++ localization/quanta_ru.ts
@@ -0,0 +1,169 @@
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Main</name>
+    <message>
+        <source>Main</source>
+        <translation>Главная</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+	<message>
+        <source>Parameters</source>
+        <translation>Параметры</translation>
+    </message>
+	<message>
+        <source>About</source>
+        <translation>Об авторе</translation>
+    </message>
+
+    <message>
+        <source>Cleared</source>
+        <translation>Можно очистить ~ </translation>
+    </message>
+    <message>
+        <source>WaitingTask</source>
+        <translation>Ожидание задач...</translation>
+    </message>
+
+
+    <message>
+        <source>ClearedTemporary</source>
+        <translation>Очищено временных файлов на</translation>
+    </message>
+    <message>
+    	<source>AdminWarning</source>
+  	<translation>Запустите от имени
+администратора</translation>
+    </message>
+    <message>
+    	<source>WinSXS</source>
+  	<translation>Очищено в WinSxS на</translation>
+    </message>
+    <message>
+    	<source>ClearedWinTemp</source>
+  	<translation>Очищено в WinTemp на</translation>
+    </message>
+    <message>
+    	<source>ClearedFontCache</source>
+  	<translation>Очищено fontcache файлов на</translation>
+    </message>
+    <message>
+    	<source>ClearedBin</source>
+  	<translation>Очищена корзина на</translation>
+    </message>
+    <message>
+    	<source>ClearedUpdate</source>
+  	<translation>Очищен кеш обновлений на</translation>
+    </message>
+    <message>
+    	<source>ClearedEvent</source>
+  	<translation>Очищено кеша событий на</translation>
+    </message>
+    <message>
+    	<source>ClearedDump</source>
+  	<translation>Очищено дампа ошибок на</translation>
+    </message>
+    <message>
+    	<source>ClearedPoint</source>
+  	<translation>Очищено мусора с
+точек восстановления на</translation>
+    </message>
+
+
+</context>
+
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <source>ThisLang</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <source>ThemeMode</source>
+        <translation>Оформление</translation>
+    </message>
+    <message>
+        <source>DarkTheme</source>
+        <translation>Тёмная тема</translation>
+    </message>
+    <message>
+        <source>LightTheme</source>
+        <translation>Светлая тема</translation>
+    </message>
+    <message>
+        <source>Animations</source>
+        <translation>Анимации</translation>
+    </message>
+    <message>
+        <source>Autoloading</source>
+        <translation>Автозагрузка</translation>
+    </message>
+   <message>
+        <source>Report</source>
+        <translation>Сообщить об ошибке</translation>
+    </message>
+    <message>
+        <source>Notification</source>
+        <translation>Расположение уведомлений</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Режим откладки</translation>
+    </message>
+    <message>
+        <source>ChooseThemeMode</source>
+        <translation>Выберите оформление</translation>
+    </message>
+    <message>
+        <source>ChooseLang</source>
+        <translation>Выберите язык</translation>
+    </message>
+    <message>
+        <source>Copied</source>
+        <translation>Успешно скопировано!</translation>
+    </message>
+    <message>
+        <source>FindError</source>
+        <translation>Нашли ошибку?</translation>
+    </message>
+    <message>
+        <source>Support</source>
+        <translation>Обратитесь в поддержку</translation>
+    </message>
+</context>
+
+
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>WaitingTask</source>
+        <translation>Ожидание задач...</translation>
+    </message>
+    <message>
+        <source>CleaningAnalysis</source>
+        <translation>Анализ очистки...</translation>
+    </message>
+</context>
+
+
+<context>
+    <name>ParametersPage</name>
+    <message>
+        <source>Info</source>
+        <translation>Инфо</translation>
+    </message>
+</context>
+
+
+</TS>
+
