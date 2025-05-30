42c4bcdc393abb523660e52afb6f6aada42283b9
Unreallx <artemx2224@yandex@gmail.com>
2025-05-30 12:00:13 +0300

    upd build

--- build/CMakeFiles/appQuantaCleaner_qmllint_json.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/appQuantaCleaner_qmllint_json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appQuantaCleaner_qmllint_json.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
