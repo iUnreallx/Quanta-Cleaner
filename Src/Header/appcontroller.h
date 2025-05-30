5cb56b8555a0c5bc25040c8a2f12000a01a2e9f6
Unreallx <artemx2224@yandex@gmail.com>
2025-05-30 11:48:28 +0300

    add localization support

--- Src/Header/appcontroller.h
+++ Src/Header/appcontroller.h
@@ -2,14 +2,27 @@
 #define APPCONTROLLER_H
 
 #include <QObject>
+#include <QTranslator>
 
 class AppController : public QObject {
     Q_OBJECT
+    Q_PROPERTY(int languageVersion READ languageVersion NOTIFY languageVersionChanged)
+
 public:
     explicit AppController(QObject *parent = nullptr);
+    int languageVersion() const { return m_languageVersion; }
 
     Q_INVOKABLE void addToAutostart();
     Q_INVOKABLE void removeFromAutostart();
+    Q_INVOKABLE void setLanguage(const QString &lang);
+
+signals:
+    void languageChanged();
+    void languageVersionChanged();
+
+private:
+    QTranslator m_translator;
+    int m_languageVersion = 0;
 };
 
 #endif // APPCONTROLLER_H
