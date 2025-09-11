15dc2c27edca44164ac8bd186cbe2cd873d537cf
Unreallx <artemx2224@yandex@gmail.com>
2025-09-11 18:25:43 +0300

    rm

--- build/Desktop_Qt_6_6_3_MinGW_64_bit-Release/CMakeFiles/appQuantaCleaner.dir/build/Desktop_Qt_6_6_3_MinGW_64_bit-Release/.rcc/qrc_qmake_QuantaCleaner.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appQuantaCleaner.dir/build/Desktop_Qt_6_6_3_MinGW_64_bit-Release/.rcc/qrc_qmake_QuantaCleaner.cpp.obj: \
- D:\qt_apps\QuantaCleaner\build\Desktop_Qt_6_6_3_MinGW_64_bit-Release\.rcc\qrc_qmake_QuantaCleaner.cpp
